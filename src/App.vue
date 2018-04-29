d81d5b72b3b7092ec0fa300e7241592f6b0c740e
Drew Sullivan <drew.sullivan.dma@gmail.com>
2018-04-29 16:57:17 -0400

    Added firebase

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div id="app" class="ui container">
-    <router-view/>
+    <bball stats="stats"></bball>
+    <div v-for="(item, index) in stats" :key="index">{{ item }}</div>
   </div>
 </template>
 
 <script>
+import BBall from './components/BBall'
+import Firebase from 'firebase'
+let config = {
+    apiKey: "AIzaSyBqSI9c69b6k8-ozjv9lxmrvm9dAghgstc",
+    authDomain: "free-throw-pro.firebaseapp.com",
+    databaseURL: "https://free-throw-pro.firebaseio.com",
+    storageBucket: "free-throw-pro.appspot.com",
+    messagingSenderId: "607926992175"
+  };
+let app = Firebase.initializeApp(config)
+let db = app.database()
+let statsRef = db.ref('stats')
+
 export default {
-  name: 'App'
+  name: 'App',
+  firebase: {
+    stats: statsRef
+  },
+  components: {
+    bball: BBall
+  }
 }
 </script>
 
--- src/components/BBall.vue
+++ src/components/BBall.vue
@@ -15,7 +15,7 @@
       <tbody>
         <tr>
           <td>Lifetime</td>
-          <td>{{ avgOf10 }}%</td>
+          <td>{{ avgOf10 }}</td>
         </tr>
         <tr>
           <td>{{ stats[0].date }}</td>
@@ -77,23 +77,28 @@
 <script>
 export default {
   name: 'BBall',
+  props: ['stats'],
   created: function () {
-    this.stats.sort(byDate)
+    return
+    // this.stats.array.forEach(element => {
+    //   console.log(element)
+    // });
+    // this.stats.sort(byDate)
   },
   data () {
     return {
       adding: false,
-      stats: [
-        { date: '2018-04-26', legit: 12, of10: 4, short: 12, long: 14, left: 15, right: 6 },
-        { date: '2018-04-24', legit: 13, of10: 3, short: 13, long: 12, left: 14, right: 7 },
-        { date: '2018-04-25', legit: 14, of10: 2, short: 14, long: 11, left: 13, right: 8 },
-        { date: '2018-04-23', legit: 15, of10: 1, short: 15, long: 10, left: 8, right: 8 },
-        { date: '2018-04-24', legit: 14, of10: 0, short: 16, long: 9, left: 9, right: 9 },
-        { date: '2018-04-22', legit: 13, of10: 2, short: 17, long: 8, left: 10, right: 12 },
-        { date: '2018-04-18', legit: 12, of10: 4, short: 18, long: 12, left: 10, right: 13 },
-        { date: '2018-04-23', legit: 11, of10: 5, short: 9, long: 13, left: 12, right: 9 },
-        { date: '2018-04-20', legit: 14, of10: 2, short: 8, long: 13, left: 11, right: 8 }
-      ],
+      // stats: [
+      //   { date: '2018-04-26', legit: 12, of10: 4, short: 12, long: 14, left: 15, right: 6 },
+      //   { date: '2018-04-24', legit: 13, of10: 3, short: 13, long: 12, left: 14, right: 7 },
+      //   { date: '2018-04-25', legit: 14, of10: 2, short: 14, long: 11, left: 13, right: 8 },
+      //   { date: '2018-04-23', legit: 15, of10: 1, short: 15, long: 10, left: 8, right: 8 },
+      //   { date: '2018-04-24', legit: 14, of10: 0, short: 16, long: 9, left: 9, right: 9 },
+      //   { date: '2018-04-22', legit: 13, of10: 2, short: 17, long: 8, left: 10, right: 12 },
+      //   { date: '2018-04-18', legit: 12, of10: 4, short: 18, long: 12, left: 10, right: 13 },
+      //   { date: '2018-04-23', legit: 11, of10: 5, short: 9, long: 13, left: 12, right: 9 },
+      //   { date: '2018-04-20', legit: 14, of10: 2, short: 8, long: 13, left: 11, right: 8 }
+      // ],
       date: '',
       legit: 0,
       of10: 0,
@@ -105,7 +110,8 @@ export default {
   },
   computed: {
     avgOf10: function () {
-      return getAvg(this.stats, 'of10') * 10
+      console.log(getAvg(this.stats, 'of10'))
+      return `${getAvg(this.stats, 'of10') * 10}%`
     },
     classObject_legit: function () {
       return {
