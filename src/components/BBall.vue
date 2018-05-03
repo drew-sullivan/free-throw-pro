09c6ab23b11d256e54457dce1f49184040b305cf
dsullivan <drew.sullivan@hyland.com>
2018-05-03 14:51:43 -0400

    Broke out js from bball template

--- src/components/BBall.vue
+++ src/components/BBall.vue
@@ -42,7 +42,6 @@
       <button @click="cancel" class="btn btn-secondary mobile-button">Cancel</button>
       <button @click="add" class="btn btn-primary mobile-button">Submit</button>
     </div>
-    <div>{{ focusList }}</div>
     <table class="table table-striped">
       <thead>
         <tr>
@@ -88,93 +87,8 @@
   </div>
 </template>
 
-<script>
-import AvgChart from './AvgChart'
-import HelperShotsChart from './HelperShotsChart.vue'
+<script src="./bball.js">
 
-export default {
-  name: 'BBall',
-  props: ['stats'],
-  components: {
-    avgChart: AvgChart,
-    helperShotsChart: HelperShotsChart
-  },
-  data () {
-    return {
-      adding: false,
-      date: '',
-      legit: 0,
-      of10: 0,
-      short: 0,
-      long: 0,
-      left: 0,
-      right: 0
-    }
-  },
-  computed: {
-    freeThrowAverage: function () {
-      return this.getAvg(this.stats, 'of10') * 10
-    },
-    statsLen: function () {
-      return this.stats.length - 1
-    },
-    sortedStats: function () {
-      return this.stats.slice().sort((a, b) => new Date(b.date) - new Date(a.date))
-    },
-    runningAverages: function () {
-      const of10Arr = Array.from(this.sortedStats, x => +x.of10).reverse()
-      const runningAverages = []
-      for (let i = 1; i <= of10Arr.length; i++) {
-        let currentArray = of10Arr.slice(0, i)
-        runningAverages.push(+(currentArray.reduce((acc, curr) => acc + curr) / currentArray.length).toFixed(2))
-      }
-      return runningAverages
-    }
-  },
-  methods: {
-    add: function () {
-      const newData = {
-        date: this.date,
-        legit: this.legit,
-        of10: this.of10,
-        short: this.short,
-        long: this.long,
-        left: this.left,
-        right: this.right
-      }
-      this.toggle()
-      this.$emit('add-new-data', newData)
-    },
-    cancel: function () {
-      this.toggle()
-    },
-    toggle: function () {
-      this.adding = !this.adding
-    },
-    getColorStatus: function (prop) {
-      const stat = this.sortedStats[0][prop]
-      const avg = this.getAvg(this.stats, prop)
-      if (prop === 'of10') {
-        return {
-          positive: +stat > +avg,
-          negative: +stat <= +avg
-        }
-      } else {
-        return {
-          positive: +stat < +avg,
-          negative: +stat >= +avg
-        }
-      }
-    },
-    getAvg: function (arr, prop) {
-      return (arr.reduce((acc, stat) => acc + +stat[prop], 0) / arr.length).toFixed(2)
-    },
-    shortDate: function (date) {
-      const dateItems = date.split('-')
-      return dateItems.slice(1).join('/')
-    }
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
