91421621fcc5072302a8456004d805fa4165efd7
dsullivan <drew.sullivan@hyland.com>
2018-05-04 12:42:04 -0400

    Fixed error where data was not building correctly

--- src/components/BBall.vue
+++ src/components/BBall.vue
@@ -46,10 +46,10 @@
       <thead>
         <tr>
           <th></th>
-          <th v-for="(shotType, i) in shotTypes" :key="i">{{ shotType | caps }}</th>
+          <th v-for="(shotType, i) in shotTypes" :key="i" class="title">{{ shotType | caps }}</th>
         </tr>
         <tr>
-          <th>AVG</th>
+          <th class="title">Avg</th>
           <th v-for="(shotType, i) in shotTypes" :key="i">{{ getAvg(sortedStats, shotType) }}</th>
         </tr>
       </thead>
@@ -69,5 +69,171 @@
   </div>
 </template>
 
-<script src="./bball.js"></script>
-<style scoped src="./bball.css"></style>
+<script>
+import AvgChart from './AvgChart'
+import HelperShotsChart from './HelperShotsChart.vue'
+
+export default {
+  name: 'BBall',
+  props: ['stats'],
+  components: {
+    avgChart: AvgChart,
+    helperShotsChart: HelperShotsChart
+  },
+  data () {
+    return {
+      shotTypes: ['of10', 'short', 'long', 'left', 'right', 'legit'],
+      adding: false,
+      date: '',
+      legit: 0,
+      of10: 0,
+      short: 0,
+      long: 0,
+      left: 0,
+      right: 0
+    }
+  },
+  computed: {
+    freeThrowAverage: function () {
+      return this.getAvg(this.stats, 'of10') * 10
+    },
+    statsLen: function () {
+      return this.stats.length - 1
+    },
+    sortedStats: function () {
+      return this.stats.slice().sort((a, b) => new Date(b.date) - new Date(a.date))
+    },
+    runningAverages: function () {
+      const of10Arr = Array.from(this.sortedStats, x => +x.of10).reverse()
+      const runningAverages = []
+      for (let i = 1; i <= of10Arr.length; i++) {
+        let currentArray = of10Arr.slice(0, i)
+        runningAverages.push(+(currentArray.reduce((acc, curr) => acc + curr) / currentArray.length).toFixed(2))
+      }
+      return runningAverages
+    }
+  },
+  methods: {
+    add: function () {
+      const newData = {
+        date: this.date,
+        legit: this.legit,
+        of10: this.of10,
+        short: this.short,
+        long: this.long,
+        left: this.left,
+        right: this.right
+      }
+      this.toggle()
+      this.$emit('add-new-data', newData)
+      location.reload()
+    },
+    cancel: function () {
+      this.toggle()
+    },
+    toggle: function () {
+      this.adding = !this.adding
+    },
+    getColorStatus: function (prop) {
+      const stat = this.sortedStats[0][prop]
+      const avg = this.getAvg(this.stats, prop)
+      if (prop === 'of10') {
+        return {
+          positive: +stat > +avg,
+          negative: +stat <= +avg
+        }
+      } else {
+        return {
+          positive: +stat < +avg,
+          negative: +stat >= +avg
+        }
+      }
+    },
+    getAvg: function (arr, prop) {
+      return (arr.reduce((acc, stat) => acc + +stat[prop], 0) / arr.length).toFixed(2)
+    }
+  },
+  filters: {
+    caps: function (str) {
+      if (str === 'of10') {
+        return 'Of 10'
+      }
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    },
+    shortDate: function (dateString) {
+      const dateItems = dateString.split('-')
+      return dateItems.slice(1).join('/')
+    }
+  }
+}
+</script>
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.positive {
+  background-color: #D0E7D0;
+}
+
+.negative {
+  background-color: #FFD0D0;
+}
+
+label {
+  float: left;
+}
+
+.top-label {
+  font-size: 20px;
+  color: #878787;
+  margin: 20px auto -10px auto !important;
+}
+
+.util-margin-20 {
+  margin: 20px auto;
+}
+
+.mobile-button {
+  margin: 5px auto;
+  width: 100%;
+  height: 60px;
+  font-size: 35px;
+}
+
+input[type="date"] {
+  display:block;
+  -webkit-appearance: textfield;
+  -moz-appearance: textfield;
+  min-height: 1.2em;
+}
+
+select, textarea, input[type="text"], input[type="password"],
+input[type="datetime"], input[type="datetime-local"],
+input[type="date"], input[type="month"], input[type="time"],
+input[type="week"], input[type="number"], input[type="email"],
+input[type="url"], input[type="search"], input[type="tel"],
+input[type="color"] {
+  font-size: 16px;
+}
+
+.stat-chart {
+  margin: 30px auto;
+}
+
+/* .title {
+  font-size: 10px;
+  letter-spacing: 1px;
+} */
+</style>
