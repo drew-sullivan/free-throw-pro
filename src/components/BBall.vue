21a8e37d3ea33d3fa5ce2d755bc12545a3fdbff7
Drew Sullivan <drew.sullivan.dma@gmail.com>
2018-06-11 18:39:32 -0400

    Paring down to just LeBron, and adding var to view

--- src/components/BBall.vue
+++ src/components/BBall.vue
@@ -5,6 +5,8 @@
       <h1>{{ freeThrowAverage }}%</h1>
     </div>
 
+    <p>LeBron's Average: {{ lebronJamesFreeThrowAverage }}%</p>
+
     <h2>
       <i v-if="progress > 0" class="fa fa-arrow-up"></i>
       <i v-else class="fa fa-arrow-down"></i>
@@ -120,9 +122,7 @@ export default {
       adding: false,
       date: '',
       of10: 0,
-      short: 0,
-      long: 0,
-      closestNBAPlayer: ''
+      lebronJamesFreeThrowAverage: 0
     }
   },
   mounted () {
@@ -135,38 +135,13 @@ export default {
         'Authorization': 'Basic ' + btoa(`${USERNAME}:${PASSWORD}`)
       },
       success: function (data) {
-        // const stats = JSON.stringify(data, null, '\t')
-        const playerStats = data['cumulativeplayerstats']['playerstatsentry']
-        const getFreeThrowAvg = (player) => {
-          const freeThrowAttempts = +player.stats['FtAtt']['#text']
-          const freeThrowSuccesses = +player.stats['FtMade']['#text']
-          return (freeThrowSuccesses / freeThrowAttempts * 100).toFixed(2)
-        }
-        const playersSortedByFreeThrowAverage = playerStats.sort(
-          (playerA, playerB) => {
-            const playerAFreeThrowAvg = getFreeThrowAvg(playerA)
-            const playerBFreeThrowAvg = getFreeThrowAvg(playerB)
-            return playerBFreeThrowAvg - playerAFreeThrowAvg
-          }
-        )
-        playersSortedByFreeThrowAverage.forEach(item => {
-          const player = item.player
-          const freeThrowAvg = getFreeThrowAvg(item)
-          const freeThrowAttempts = +item.stats['FtAtt']['#text']
-          const freeThrowSuccesses = +item.stats['FtMade']['#text']
-          if (+item.stats['FtAtt']['#text'] && freeThrowAttempts >= 30) {
-            console.log(`${player.FirstName} ${player.LastName} - ${freeThrowAvg}% - ${freeThrowSuccesses}/${freeThrowAttempts}`)
-          }
-        })
-
-        for (let item of playersSortedByFreeThrowAverage) {
-          console.log(this.freeThrowAverage)
-          console.log(getFreeThrowAvg(item))
-          if (this.freeThrowAverage <= getFreeThrowAvg(item)) {
-            console.log('\n\n\n')
-            console.log(`DREW!!! ${item}, getFreeThrowAvg(item)`)
-          }
-        }
+        const lebronJames = data['cumulativeplayerstats']['playerstatsentry'][0]
+        const freeThrowsAttempted = lebronJames.stats['FtAtt']['#text']
+        const freeThrowsMade = lebronJames.stats['FtMade']['#text']
+        const freeThrowAverage = (freeThrowsMade / freeThrowsAttempted * 100).toFixed(2)
+        // console.log(`LeBron's free throw average: ${freeThrowAverage}%`)
+        this.lebronJamesFreeThrowAverage = freeThrowAverage
+        console.log(this.lebronJamesFreeThrowAverage)
       }
     })
   },
